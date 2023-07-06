25c9e5dba4f1fca8f9c20fc86257216f63744162
Muirghen <vasquezmariano01@gmail.com>
2023-07-06 17:31:33 -0400

    Cleaned code and dependencies

--- screens/startingScreen.js
+++ screens/startingScreen.js
@@ -1,14 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
 import {
   StyleSheet,
-  Text,
   View,
   Image,
-  TextInput,
-  TouchableOpacity,
 } from "react-native";
 import { Button } from 'react-native-paper';
 
@@ -17,21 +12,16 @@ export default function App({navigation}) {
     <View style={styles.container}>
       <Image style={styles.image} source={require("../assets/logo-no-background.png")} />
         <StatusBar style="auto" />
-        {/* <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Login')}>
-            <Text style={styles.loginText}>Login</Text>
-        </TouchableOpacity> */}
-        {/* <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Register')}>
-            <Text style={styles.loginText}>Sign up</Text>
-        </TouchableOpacity> */}
-                <Button
-          style={{ marginTop: 15, fullWidth: true, width: '70%'}}
+        <Button
+          style={styles.button}
           icon='send'
           mode='contained'
           onPress={() => navigation.navigate('Login')}
-          width='80%'
-          >Log in</Button>
+          width='80%'>
+            Log in
+        </Button>
         <Button
-          style={{ marginTop: 25, fullWidth: true, width: '70%'}}
+          style={styles.button}
           icon='send'
           mode='contained'
           onPress={() => navigation.navigate('Register')}
@@ -41,11 +31,6 @@ export default function App({navigation}) {
 }
 
 const styles = StyleSheet.create({
-    // loginText: {
-    //     color: "#fff",
-    //     fontWeight: 'bold',
-    //     alignSelf: 'center',
-    // },
   container: {
     flex: 1,
     flexDirection: 'column',
@@ -58,14 +43,9 @@ const styles = StyleSheet.create({
     width: '95%',
     resizeMode: 'contain',
   },
-  // loginBtn: {
-  //   width: "80%",
-  //   borderRadius: 25,
-  //   height: 50,
-  //   justifyContent: "center",
-  //   marginTop: 25,
-  //   marginBottom: 10,
-  //   backgroundColor: "#6563DB",
-  //   alignItems: "stretch",
-  // },
+  button: {
+    marginTop: 25, 
+    fullWidth: true, 
+    width: '70%',
+  }
 });
