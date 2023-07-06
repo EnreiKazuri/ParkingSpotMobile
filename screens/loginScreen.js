a12ebf5839e1d68caf8c0200bb901ba7e29f9175
Muirghen <vasquezmariano01@gmail.com>
2023-07-06 17:30:38 -0400

    Cleaned code and dependencies

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,27 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
   StyleSheet,
-  //Text,
   View,
   Image,
-  TextInput as TextInputNative,
-  //Button,
-  TouchableOpacity,
   BackHandler,
-  Alert,
-  FlatList
 } from "react-native";
-import { TextInput,
+import { 
+  TextInput,
   Snackbar,
   Text,
   Button,
 } from "react-native-paper";
 import Icon from 'react-native-paper/src/components/Icon'
-// import { useSnackbar } from "notistack";
-//import { Alert as SnackAlert, Snackbar } from '@mui/material';
 import axios from 'axios';
 
 export default function App({navigation}) {
@@ -30,7 +21,6 @@ export default function App({navigation}) {
   const [visible, setVisible] = useState(false);
   const onToggleSnackBar = () => setVisible(true);
   const onDismissSnackBar = () => setVisible(false);
-  // const { enqueueSnackbar } = useSnackbar();
   const [customAlert, setCustomAlert] = useState({
     severity: "error",
     message: "placeHolder",
@@ -38,14 +28,6 @@ export default function App({navigation}) {
 
   useEffect(() => {
     const backAction = () => {
-      // Alert.alert('Hold on!', 'Are you sure you want to go back?', [
-      //   {
-      //     text: 'Cancel',
-      //     onPress: () => null,
-      //     style: 'cancel',
-      //   },
-      //   {text: 'YES', onPress: () => navigation.goBack()},
-      // ]);
       navigation.goBack();
       return true;
     };
@@ -81,47 +63,21 @@ export default function App({navigation}) {
     <View style={styles.container}>
       <Image style={styles.image} source={require("../assets/parkingspot_crop_logo.png")} />
       <StatusBar style="auto" />
-      {/* <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email"
-          placeholderTextColor="#2A295C"
-          onChangeText={(email) => setEmail(email)}
-        />
-      </View> */}
       <TextInput
-        style={{ marginTop: 15, fullWidth: true, width: '70%'}}
+        style={styles.reducedMarginBtn}
         label='Email'
         mode='outlined'
         onChangeText={(email) => setEmail(email)}
         />
-      
-      {/* <View style={styles.inputView}>
-        <TextInputNative
-          style={styles.TextInput}
-          placeholder="Password"
-          placeholderTextColor="#2A295C"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View> */}
       <TextInput
-        style={{ marginTop: 15, fullWidth: true, width: '70%'}}
+        style={styles.reducedMarginBtn}
         label='Password'
         mode='outlined'
         onChangeText={(password) => setPassword(password)}
         />
-      {/* <TouchableOpacity>
-        {/* <Text style={styles.forgot_button} onPress to recover  >Forgot Password?</Text>
-       </TouchableOpacity> */}
       <Text style={{marginTop: 15, color: '#6563DB'}}>Forgot Password?</Text>
-      {/* <TouchableOpacity style={styles.loginBtn} onPress={() => SendToBackend()}>
-        <Text style={styles.loginText}>Login</Text>
-      </TouchableOpacity> */}
       <Button
-          style={{ marginTop: 15, fullWidth: true, width: '70%'}}
-          //labelStyle={{fontSize: 15, color: '#fff'}}
-          //contentStyle={{height: 50}}
+          style={styles.reducedMarginBtn}
           mode='contained'
           onPress={() => SendToBackend()}
           width='80%'
@@ -158,38 +114,14 @@ const styles = StyleSheet.create({
     width: '95%',
     resizeMode: 'contain',
   },
-  inputView: {
-    backgroundColor: "#AAA9E1",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "stretch",
+  button: {
+    marginTop: 25, 
+    fullWidth: true, 
+    width: '70%',
+  },
+  reducedMarginBtn: {
     marginTop: 15,
-  },
-  TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    textAlign: "center",
-  },
-  forgot_button: {
-    height: 20,
-    marginBottom: 20,
-    color: "#6563DB",
-  },
-  loginText: {
-    color: "#fff",
-    fontWeight: 'bold',
-},
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 25,
-    marginBottom: 10,
-    backgroundColor: "#6563DB",
-  },
+    fullWidth: true, 
+    width: '70%',
+  }
 });
