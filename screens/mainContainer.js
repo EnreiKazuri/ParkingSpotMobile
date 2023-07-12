ac500dbdc24b298b0e7a6908611dd483b1c28d89
Muirghen <52637101+Muirghen@users.noreply.github.com>
2023-07-11 20:07:25 -0400

    Bottom Navigation is now working

--- screens/mainContainer.js
+++ screens/mainContainer.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
-import { BottomNavigation, Text } from 'react-native-paper';
+import { Text } from 'react-native-paper';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import AgendaScreen from './mainScreens/agendaScreen.js';
 import ProfileScreen from './mainScreens/profileScreen.js';
@@ -9,21 +10,21 @@ import SettingsScreen from './mainScreens/settingsScreen.js';
 import MainScreen from './mainScreens/mainScreen.js';
 
 export default function MainContainer(){
-    const navigationItems = [
-        { key: 'home', title: 'Home', icon: 'home' },
-        { key: 'agenda', title: 'Agenda', icon: 'calendar'},
-        { key: 'profile', title: 'Profile', icon: 'account' },
-        { key: 'settings', title: 'Settings', icon: 'cog' },
-      ];
-    const sceneMap = {
-        home: MainScreen,
-        agenda: AgendaScreen,
-        profile: ProfileScreen,
-        settings: SettingsScreen,
-    };
-    const [selectedItem, setSelectedItem] = React.useState(navigationItems[0].key);
-    const renderScreen = BottomNavigation.SceneMap(sceneMap);
-    
+    // const navigationItems = [
+    //     { key: 'home', title: 'Home', icon: 'home' },
+    //     { key: 'agenda', title: 'Agenda', icon: 'calendar'},
+    //     { key: 'profile', title: 'Profile', icon: 'account' },
+    //     { key: 'settings', title: 'Settings', icon: 'cog' },
+    //   ];
+    // const sceneMap = {
+    //     home: MainScreen,
+    //     agenda: AgendaScreen,
+    //     profile: ProfileScreen,
+    //     settings: SettingsScreen,
+    // };
+    // const [selectedItem, setSelectedItem] = React.useState(navigationItems[0].key);
+    // const renderScreen = BottomNavigation.SceneMap(sceneMap);
+    const Tab = createMaterialBottomTabNavigator();
     return(
         <View style={{ flex: 1 }}>
             {/* {renderScreen()}
@@ -32,11 +33,17 @@ export default function MainContainer(){
             onIndexChange={index => setSelectedItem(navigationItems[index].key)}
             renderScene={BottomNavigation.SceneMap(renderScreen)}
             /> */}
-            {renderScene}
+            {/* {renderScene}
             <BottomNavigation
                 navigationState={{ index: navigationItems.findIndex(item => item.key === selectedItem), routes: navigationItems }}
                 onIndexChange={index => setSelectedItem(navigationItems[index].key)}
-                renderScene={renderScene} />
+                renderScene={renderScene} /> */}
+            <Tab.Navigator>
+            <Tab.Screen name="Home" component={MainScreen} options={{tabBarIcon: 'home'}} />
+            <Tab.Screen name="Agenda" component={AgendaScreen} options={{tabBarIcon: 'view-agenda'}}/>
+            <Tab.Screen name="Profile" component={ProfileScreen} options={{tabBarIcon: 'account'}}/>
+            <Tab.Screen name="Settings" component={SettingsScreen} options={{tabBarIcon: 'cog'}}/>
+            </Tab.Navigator>
         </View>
     );
 }
