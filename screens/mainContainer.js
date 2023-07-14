3586a6c643b562ce074ab276c73a204f8ae9e3fd
Muirghen <vasquezmariano01@gmail.com>
2023-07-13 21:41:41 -0400

    Eliminated acces to settings from navigation. Cleaned code

--- screens/mainContainer.js
+++ screens/mainContainer.js
@@ -10,39 +10,13 @@ import SettingsScreen from './mainScreens/settingsScreen.js';
 import MainScreen from './mainScreens/mainScreen.js';
 
 export default function MainContainer(){
-    // const navigationItems = [
-    //     { key: 'home', title: 'Home', icon: 'home' },
-    //     { key: 'agenda', title: 'Agenda', icon: 'calendar'},
-    //     { key: 'profile', title: 'Profile', icon: 'account' },
-    //     { key: 'settings', title: 'Settings', icon: 'cog' },
-    //   ];
-    // const sceneMap = {
-    //     home: MainScreen,
-    //     agenda: AgendaScreen,
-    //     profile: ProfileScreen,
-    //     settings: SettingsScreen,
-    // };
-    // const [selectedItem, setSelectedItem] = React.useState(navigationItems[0].key);
-    // const renderScreen = BottomNavigation.SceneMap(sceneMap);
     const Tab = createMaterialBottomTabNavigator();
     return(
         <View style={{ flex: 1 }}>
-            {/* {renderScreen()}
-            <BottomNavigation
-            navigationState={{ index: navigationItems.findIndex(item => item.key === selectedItem), routes: navigationItems }}
-            onIndexChange={index => setSelectedItem(navigationItems[index].key)}
-            renderScene={BottomNavigation.SceneMap(renderScreen)}
-            /> */}
-            {/* {renderScene}
-            <BottomNavigation
-                navigationState={{ index: navigationItems.findIndex(item => item.key === selectedItem), routes: navigationItems }}
-                onIndexChange={index => setSelectedItem(navigationItems[index].key)}
-                renderScene={renderScene} /> */}
             <Tab.Navigator>
             <Tab.Screen name="Home" component={MainScreen} options={{tabBarIcon: 'home'}} />
             <Tab.Screen name="Agenda" component={AgendaScreen} options={{tabBarIcon: 'view-agenda'}}/>
             <Tab.Screen name="Profile" component={ProfileScreen} options={{tabBarIcon: 'account'}}/>
-            <Tab.Screen name="Settings" component={SettingsScreen} options={{tabBarIcon: 'cog'}}/>
             </Tab.Navigator>
         </View>
     );
