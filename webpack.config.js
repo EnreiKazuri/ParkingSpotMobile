5e8617f6bb520ee5f49f1190a91f5bbe1be205a3
Muirghen <vasquezmariano01@gmail.com>
2023-07-14 14:59:52 -0400

    Changes

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  // Customize the config before returning it.
+  config.resolve.alias['react-native'] = 'react-native-web';
+  config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
+  plugins: [
+  
+  ];
+  return config;
+};
