4ff7729e4935cdca532b64271e90b0c45b35cd9a
Muirghen <vasquezmariano01@gmail.com>
2023-07-13 21:53:43 -0400

    Revised functionality of profileScreen

--- screens/mainScreens/profileScreen.js
+++ screens/mainScreens/profileScreen.js
@@ -1,30 +1,80 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { View, StyleSheet, Image } from "react-native";
+import { List, Divider, Text, Button, Snackbar } from 'react-native-paper'
+import Icon from 'react-native-paper/src/components/Icon'
 
-export default function ProfileScreen() {
+export default function ProfileScreen({navigation}) {
   const [avatar, setAvatar] = useState("");
   const [name, setName] = useState("");
   const [lastname, setLastname] = useState("");
+  const [visible, setVisible] = useState(false);
+  const onToggleSnackBar = () => setVisible(true);
+  const onDismissSnackBar = () => setVisible(false);
 
-  const handleAddCar = () => {
-    console.log("Add car");
+  const user = {
+    name: "Juan",
+    lastname: "Perez",
+    email: "peresitoeljuanito@gmail.com",
+    phone: "123-456-7890",
   };
+
   return (
     <View style={styles.container}>
-      <Image source={{ uri: avatar }} style={styles.avatar} />
-      <Text style={styles.name}>Juan</Text>
-      <Text style={styles.lastname}>Perez</Text>
-      <View style={styles.separator} />
-      <Text style={styles.sectionTitle}>Vehicle</Text>
-      <Text style={styles.skills}>Honda Civic</Text>
+      {/* <Image source={{ uri: avatar }} style={styles.avatar} /> */}
+      <Icon source="account-circle" color="black" size={70} />
+      <Text style={styles.name}>{user.name + " " + user.lastname}</Text>
+      {/* <Text style={styles.lastname}>Perez</Text> */}
+      <Button
+        style={styles.button}
+        labelStyle={{color: '#fff', fontWeight: 'bold', fontSize: 15}}
+        mode='contained'
+        onPress={() => navigation.navigate('EditProfile', user)}
+        width='80%'>
+        Edit Profile
+      </Button>
       <View style={styles.separator} />
       <Text style={styles.sectionTitle}>Contact</Text>
-      <Text style={styles.contact}>Email: example@example.com</Text>
-      <Text style={styles.contact}>Phone: 123-456-7890</Text>
-      <TouchableOpacity onPress={handleAddCar} style={styles.addButton}>
-        <Text style={styles.addButtonLabel}>Add Car</Text>
-      </TouchableOpacity>
+      <Text style={styles.contact}>{"Email: " + user.email}</Text>
+      <Text style={styles.contact}>{"Phone: " + user.phone}</Text>
+      <View style={styles.separator} />      
+      <View style={styles.containerRecent}>
+      <List.Item
+        key={1}
+        title="See my cars"
+        left={props => <List.Icon {...props} icon="car" />}
+        titleStyle={{fontSize: 16}}
+        style={{flexWrap: 'nowrap'}}
+        onPress={() => navigation.navigate('CarList')}
+      />
+      <Divider style={{height: 1, width: "100%" }} bold={false}/>
+      <List.Item
+        key={1}
+        title="Settings"
+        left={props => <List.Icon {...props} icon="cog" />}
+        titleStyle={{fontSize: 16}}
+        style={{flexWrap: 'nowrap'}}
+        onPress={() => onToggleSnackBar()}
+      />
+      <Divider style={{height: 1, width: "100%" }} bold={false}/>
+      <List.Item
+        key={1}
+        title="About us"
+        left={props => <List.Icon {...props} icon="account-group" />}
+        titleStyle={{fontSize: 16}}
+        style={{flexWrap: 'nowrap'}}
+        onPress={() => navigation.navigate('Settings')}
+      />
+      <Divider style={{height: 1, width: "100%" }} bold={false}/>
+      </View>
+      <Snackbar 
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        style={{ backgroundColor: '#D1312A'}}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Icon source="alert-circle-outline" color="#fff" size={24} />
+          <Text style={{ marginLeft: 10, color: '#fff', fontWeight: 'bold'}}>Not available yet</Text>
+        </View>
+      </Snackbar>
     </View>
   );
 }
@@ -35,6 +85,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 16,
+    backgroundColor: "#fff",
   },
   avatar: {
     width: 150,
@@ -84,5 +135,12 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  containerRecent: {
+    width: '95%',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#fff',
+    paddingTop: 5,
   },
 });
