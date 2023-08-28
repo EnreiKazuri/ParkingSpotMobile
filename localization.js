d08016a3494d8bc3c6d9500976dfa3a5e27b2233
IvyMasterDiego <diegolobato82@gmail.com>
2023-08-27 21:35:49 -0400

    Add new translate file

---
+++ localization.js
@@ -0,0 +1,213 @@
+const translations = {
+  en: {
+    //#region Starting Screen
+    login: "Log in",
+    signup: "Sign up",
+    //#endregion
+
+    //#region Register Screen
+    register: "Register",
+    createaccount: "Create an account",
+    name: "Name",
+    email: "Email",
+    password: "Password",
+    confirmpassword: "Confirm password",
+    termscondition: "Terms and conditions",
+    next: "Next",
+    continuegoogle: "Continue with Google",
+    continuemicrosoft: "Continue with Microsoft",
+    continueapple: "Continue with Apple",
+    cancel: "Cancel",
+    //#endregion
+
+    //#region Login Screen
+    forgotPassword: "Forgot Password?",
+    reciberespuesta: "Get Answer",
+    onefieldempty: "One or more fields are empty",
+    //#endregion
+
+    //#region Main Screen
+    reservation: "Reservation",
+    recentreservation: "No recent reservations",
+    selectlocation: "Select Location",
+    //#endregion
+
+    //#region Profile Screen
+    editprofile: "Edit profile",
+    phone: "Phone",
+    seecars: "See my cars",
+    settings: "Settings",
+    aboutus: "About us",
+    notavaliable: "Not available yet",
+    //#endregion
+
+    //#region Activate User Screen
+    activateuser: "Activate User",
+    sendactivationcode: "Send Activation Code",
+    code: "code",
+    activate: "Activate",
+    activationsuccesfull: "Activation successful",
+    activationfailed: "Activation Failed",
+    invalidcode: "Invalid code",
+    //#endregion
+
+    //#region Car List Screen
+    mycars: "My Cars",
+    //#endregion
+
+    //#region New Car Screen
+    caregister: "Car Registration",
+    selectmaker: "Select Maker",
+    search: "Search...",
+    selectmodel: "Select Model",
+    licenseplate: "License Plate",
+    vehicleColor: "Vehicle Color",
+    done: "Done",
+    //#endregion
+
+    //#region Reset Password Screen
+    resetpassword: "Reset Password",
+    newPassword: "Nueva contraseña",
+    //#endregion
+
+    //#region Sign Det Screen
+    tellusmore: "Tell us more about you",
+    lastname: "Lastname",
+    phonenumber: "Phone Number",
+    continue: "Continue",
+    //#endregion
+
+    //#region Main Container Screen
+    home: "Home",
+    agenda: "Agenda",
+    profile: "Profile",
+    //#endregion
+
+    //#region forgot Password Screen
+    passwordrecovery: "Password recovery",
+    sendemail: "Send mail",
+    passwordresetsuccesfull: "Password Reset successful",
+    passwordresetfailed: "Password Reset Failed",
+    invalidemailaddress: "Invalid email address",
+    //#endregion
+
+    //#region Edit Profile Screen
+    editprofile: "Edit Profile",
+    updateprofile: "Update Profile",
+    //#endregion
+
+    //#region Settings Screen
+    equipodetrabajo: "Work team",
+    //#endregion
+
+    //#region detailed Map Screen
+    parking: "Parking",
+    disponible: "Available",
+    ocupado: "Busy",
+    //#endregion
+  },
+  es: {
+    //#region startingscreen
+    login: "Iniciar sesión",
+    signup: "Registrarse",
+    //#endregion
+
+    //#region Register Screen
+    register: "Registrarse",
+    createaccount: "Crear una cuenta",
+    name: "Nombre",
+    email: "Correo",
+    password: "Contraseña",
+    confirmpassword: "Confirmar contraseña",
+    termscondition: "Terminos y condiciones",
+    next: "Siguiente",
+    continuegoogle: "Continuar con Google",
+    continuemicrosoft: "Continuar con Microsoft",
+    continueapple: "Continuar con Apple",
+    cancel: "Cancelar",
+    //#endregion
+
+    //#region Login Screen
+    forgotPassword: "¿Olvidaste tu contraseña?",
+    reciberespuesta: "Recibe respuesta",
+    onefieldempty: "Uno o más campos están vacíos",
+    //#endregion
+
+    //#region Main Screen
+    reservation: "Reservar",
+    recentreservation: "No tiene reservas recientes",
+    selectlocation: "Selecciona una ubicacion",
+    //#endregion
+
+    //#region Profile Screen
+    editprofile: "Editar perfil",
+    phone: "Telefono",
+    settings: "Opciones",
+    aboutus: "Sobre nosotros",
+    notavaliable: "No disponible todavía",
+    //#endregion
+
+    //#region Activate User Screen
+    activateuser: "Activar usuario",
+    sendactivationcode: "Enviar Código de Activación",
+    code: "Código",
+    activate: "Activar",
+    //#endregion
+
+    //#region Car List Screen
+    mycars: "Mis carros",
+    //#endregion
+
+    //#region New Car Screen
+    caregister: "Registro de carros",
+    selectmaker: "Seleccionar fabricante",
+    search: "Buscar...",
+    selectmodel: "Seleccionar modelo",
+    licenseplate: "Placa",
+    vehicleColor: "Color del Vehiculo",
+    done: "Hecho",
+    //#endregion
+
+    //#region Reset Password Screen
+    resetpassword: "Reiniciar Contraseña",
+    newPassword: "Nueva contraseña",
+    //#endregion
+
+    //#region Sign Det Screen
+    tellusmore: "Dinos mas sobre ti",
+    lastname: "Apellido",
+    phonenumber: "Numero de telefono",
+    continue: "Continuar",
+    //#endregion
+
+    //#region Main Container Screen
+    home: "Inicio",
+    agenda: "Agenda",
+    profile: "Perfil",
+    //#endregion
+
+    //#region forgot Password Screen
+    passwordrecovery: "Recuperación de contraseña",
+    sendemail: "Enviar correo",
+    passwordresetsuccesfull: "Restablecimiento de contraseña exitosa",
+    passwordresetfailed: "Error al restablecer la contraseña",
+    invalidemailaddress: "Dirección de correo electrónico no válida",
+    //#endregion
+
+    //#region Edit Profile Screen
+    editprofile: "Editar perfil",
+    updateprofile: "Actualizar perfil",
+    //#endregion
+
+    //#region Settings Screen
+    equipodetrabajo: "Equipo de trabajo",
+    //#endregion
+
+    //#region detailed Map Screen
+    parking: "Parqueo",
+    disponible: "Disponible",
+    ocupado: "Ocupado",
+    //#endregion
+  },
+};
+export { translations };
