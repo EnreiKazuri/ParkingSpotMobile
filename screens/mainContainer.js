5ae62b75a0d89e5edc8f6a294a04badca6abb41f
Muirghen <52637101+Muirghen@users.noreply.github.com>
2023-09-15 11:13:03 -0400

    Added Axios operations to maincontainer

--- screens/mainContainer.js
+++ screens/mainContainer.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
 import { Text } from 'react-native-paper';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {IP_URL} from "@env";
+import axios from 'axios';
 
 import AgendaScreen from './mainScreens/agendaScreen.js';
 import ProfileScreen from './mainScreens/profileScreen.js';
@@ -15,12 +17,31 @@ export default function MainContainer({route, navigation}){
         email: route.params.email,
         password: route.params.password,
     };
+
+
+    useEffect(() => {
+        console.log(user);
+    }, []);
+
+    const GetAllTheData = () => {
+        const axiosUrl = `${IP_URL}user/userInfo/${user.id}`;
+        axios.get(axiosUrl, { withCredentials: true })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    }
+
     const Tab = createMaterialBottomTabNavigator();
     return(
         <View style={{ flex: 1 }}>
             <Tab.Navigator>
-            <Tab.Screen name="Home" component={MainScreen} options={{tabBarIcon: 'home'}} />
-            <Tab.Screen name="Agenda" component={AgendaScreen} options={{tabBarIcon: 'view-agenda'}}/>
+            <Tab.Screen name="Home" component={MainScreen} options={{tabBarIcon: 'home'}} 
+            initialParams={{name: user.name}}/>
+            <Tab.Screen name="Agenda" component={AgendaScreen} options={{tabBarIcon: 'view-agenda'}}
+            initialParams={{name: user.name}}/>
             <Tab.Screen name="Profile" component={ProfileScreen} options={{tabBarIcon: 'account'}}
             initialParams={{name: user.name, lastName: user.lastname, email: user.email, phone: user.phone, password: user.password}}/>
             </Tab.Navigator>
