b744cf04b86218afbdcacaa8a20b990d15041a3a
Muirghen <52637101+Muirghen@users.noreply.github.com>
2023-09-25 10:34:42 -0400

    User's car list is now taken from db

--- screens/carListScreen.js
+++ screens/carListScreen.js
@@ -1,49 +1,64 @@
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import React, {useEffect} from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
 import { FAB, IconButton, List, Divider, Text } from 'react-native-paper';
+import axios from 'axios';
+import {IP_URL} from "@env";
+import { useIsFocused } from "@react-navigation/native";
 
-export default function CarListScreen({navigation}) {
-  const carList = [
-    { value: 1, licensePlate: 'AAA-111', maker: 'Toyota', model: 'Corolla', color: 'red' },
-    { value: 2, licensePlate: 'BBB-222', maker: 'Ford', model: 'Mustang', color: 'blue'},
-    { value: 3, licensePlate: 'CCC-333', maker: 'Nissan', model: 'Altima', color: 'green'},
-    { value: 1, licensePlate: 'AAA-111', maker: 'Toyota', model: 'Corolla', color: 'red' },
-    { value: 2, licensePlate: 'BBB-222', maker: 'Ford', model: 'Mustang', color: 'blue'},
-    { value: 3, licensePlate: 'CCC-333', maker: 'Nissan', model: 'Altima', color: 'green'},
-    { value: 1, licensePlate: 'AAA-111', maker: 'Toyota', model: 'Corolla', color: 'red' },
-    { value: 2, licensePlate: 'BBB-222', maker: 'Ford', model: 'Mustang', color: 'blue'},
-    { value: 3, licensePlate: 'CCC-333', maker: 'Nissan', model: 'Altima', color: 'green'},
-    { value: 1, licensePlate: 'AAA-111', maker: 'Toyota', model: 'Corolla', color: 'red' },
-    { value: 2, licensePlate: 'BBB-222', maker: 'Ford', model: 'Mustang', color: 'blue'},
-    { value: 3, licensePlate: 'CCC-333', maker: 'Nissan', model: 'Altima', color: 'green'},
-    { value: 1, licensePlate: 'AAA-111', maker: 'Toyota', model: 'Corolla', color: 'red' },
-    { value: 2, licensePlate: 'BBB-222', maker: 'Ford', model: 'Mustang', color: 'blue'},
-    { value: 3, licensePlate: 'CCC-333', maker: 'Nissan', model: 'Altima', color: 'green'},
-    { value: 1, licensePlate: 'AAA-111', maker: 'Toyota', model: 'Corolla', color: 'red' },
-    { value: 2, licensePlate: 'BBB-222', maker: 'Ford', model: 'Mustang', color: 'blue'},
-    { value: 3, licensePlate: 'CCC-333', maker: 'Nissan', model: 'Altima', color: 'green'},
-    { value: 1, licensePlate: 'AAA-111', maker: 'Toyota', model: 'Corolla', color: 'red' },
-    { value: 2, licensePlate: 'BBB-222', maker: 'Ford', model: 'Mustang', color: 'blue'},
-    { value: 3, licensePlate: 'CCC-333', maker: 'Nissan', model: 'Altima', color: 'green'},
-  ];
+export default function CarListScreen({route, navigation}) {
+  const userID = route.params;
+  const [carList, setCarList] = React.useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused)
+      getCarList();
+  }, [isFocused]);
+
+  const getCarList = () => {
+    const axiosUrl = `${IP_URL}vehicle/byUser/${userID}`;
+    axios.get(axiosUrl, { withCredentials: true })
+    .then(response => {
+      const newCarList = [];
+      console.log(response.data);
+      response.data.body.forEach(car => {
+        newCarList.push({value: car._id, licensePlate: car.plate, maker: car.name, model: car.model, color: car.color});
+      })
+      setCarList(newCarList);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
 
   const RenderCars = () => {
-    return carList.map((item, index) => (
-      <View style={styles.containerRecent}>
-        <List.Item
-        key={index}
-        title={item.licensePlate}
-        description= {item.color + " " + item.maker + " " + item.model}
-        titleStyle={{fontSize: 16, fontWeight: 'bold'}}
-        style={{flexWrap: 'nowrap'}}
-      />
-      <Divider style={{height: 1, width: "100%" }} bold={false}/>
-      </View>
-    ));
+    if (carList.length == 0) return (
+      <View>
+        <Text style={{fontSize: 16, fontWeight: 'bold', marginTop: 50, color:'gray'}}>
+          Uh oh... no cars? {userID}
+        </Text>
+      </View>);
+    else {
+      return carList.map((item, index) => (
+        <View style={styles.containerRecent}>
+          <List.Item
+          key={index}
+          title={item.licensePlate}
+          description= {item.color + " " + item.maker + " " + item.model}
+          titleStyle={{fontSize: 16, fontWeight: 'bold'}}
+          style={{flexWrap: 'nowrap'}}
+        />
+        <Divider style={{height: 1, width: "100%" }} bold={false}/>
+        </View>
+      ));
+    }
   };
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <StatusBar style="auto" />
+      <View style={{height: '5%', backgroundColor: '#fff'}}/>
       <ScrollView style={{height: '80%', backgroundColor: '#fff'}} contentContainerStyle={{alignItems: 'center'}}>
         <Text variant='headlineLarge' style={{fontWeight: 'bold', color: '#6563DB'}}>My Cars</Text>
         {RenderCars()}
@@ -53,7 +68,7 @@ export default function CarListScreen({navigation}) {
         <FAB
           icon="car-cog"
           style={styles.fab}
-          onPress={() => navigation.navigate('NewCar')}
+          onPress={() => navigation.navigate('NewCar', userID)}
           size='large'
         />
       </View>
