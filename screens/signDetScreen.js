a745b26dbb0a947444a9c6915429301ef7fab3d9
Muirghen <52637101+Muirghen@users.noreply.github.com>
2023-09-25 10:37:25 -0400

    Preliminar update on signDetScreen

--- screens/signDetScreen.js
+++ screens/signDetScreen.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, TextInput, Button } from 'react-native-paper';
+import { en, es, registerTranslation, DatePickerModal } from 'react-native-paper-dates';
+import axios from 'axios';
+import {IP_URL} from "@env";
 
 export default function SignDetailScreen({route, navigation}) {
+  registerTranslation('es', es);
+  registerTranslation('en', en);
   let name = route.params.name;
   let email = route.params.email;
-  let password = route.params.password;
+  let userID = route.params.id;
   const [lastName, setLastName] = React.useState('');
   const [phone, setPhone] = React.useState('');
+  const [date, setDate] = React.useState(undefined);
+  const [open, setOpen] = React.useState(false);
 
-  const sendToProfile = () => {
-    navigation.navigate('Main', {name: name, lastName: lastName, email: email, phone: phone, password: password}); 
+  const onDismissSingle = React.useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  const onConfirmSingle = React.useCallback(
+    (params) => {
+      setOpen(false);
+      setDate(params.date);
+    },
+    [setOpen, setDate]
+  );
+
+  const newUserDriver = () => {
+    const userDriver = {
+      user: userID,
+      name: "name",
+      lastName: lastName,
+      //phone: phone,
+      birthDate: "03/12/2001",
+    }
+    console.log(userDriver);
+    axiosUrl = `${IP_URL}userDriver`;
+    axios.post(axiosUrl, userDriver, { withCredentials: true })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    //navigation.navigate(); 
   }
   return (
-    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1, backgroundColor: 'white'}}>
       <Text variant='headlineLarge' style={{fontWeight: 'bold', color: 'black'}}>Tell us more about you, {name}</Text>
       <TextInput
         style={{width:"95%", marginBottom: 15}}
@@ -39,10 +74,26 @@ export default function SignDetailScreen({route, navigation}) {
         mode='outlined'
         onChangeText={(phone) => setPhone(phone)}
       />
+      <Button 
+        style={{width:"95%", marginBottom: 15}}
+        onPress={() => setOpen(true)} 
+        uppercase={false} 
+        mode="outlined">
+          Fecha de nacimiento
+        </Button>
+        <DatePickerModal
+          locale="en"
+          mode="single"
+          presentationStyle='pageSheet'
+          visible={open}
+          onDismiss={onDismissSingle}
+          date={date}
+          onConfirm={onConfirmSingle}
+        />
       <Button
         style={{width:"95%", marginBottom: 15}}
         mode='contained'
-        onPress={() => sendToProfile()}>
+        onPress={() => newUserDriver()}>
           Continue
         </Button>
     </View>
