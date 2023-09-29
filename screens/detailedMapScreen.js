c9e0b213d74929dcf94948c805bfde78c06229cc
Muirghen <52637101+Muirghen@users.noreply.github.com>
2023-09-28 22:21:08 -0400

    detailedMapScreen can now be rendered

--- screens/detailedMapScreen.js
+++ screens/detailedMapScreen.js
@@ -11,28 +11,32 @@ const parkings = [
   {id : 6, available: false},
 ];
 
-const RenderParking = () => {
-  parkingSorted = parkings.sort((a, b) => b.available - a.available);
-  return parkingSorted.map((item, index) => (
-    <View style={{width: '90%', minHeight: 150,alignItems: 'center',
-     backgroundColor: item.available ? 'white' : '#e6e1e1', borderRadius: 10, padding: 15,
-     marginBottom: 5, borderColor: 'black', borderWidth: 1}} onPress={ReserveParking(item.id)}>
-      <Text style={{fontWeight: 'bold', color: 'black', alignSelf: 'flex-start'}}>
-        Parking {index + 1}
-        </Text>
-      <View style={{alignSelf: 'flex-end', alignItems: 'flex-end'}}>
-        <Text style={{color: 'black', alignSelf: 'flex-end', verticalAlign: 'bottom'}}>
-          {item.available ? 'Disponible' : 'Ocupado'}
-        </Text>
-      </View>
-    </View>
-  ));
-}
+
 
 export default function DetailedMapScreen({navigation}) {
+  
   const ReserveParking = (id) => {
     navigation.navigate("Reservation", {parkingId: id});
   }
+  
+  const RenderParking = () => {
+    parkingSorted = parkings.sort((a, b) => b.available - a.available);
+    return parkingSorted.map((item, index) => (
+      <View style={{width: '90%', minHeight: 150,alignItems: 'center',
+       backgroundColor: item.available ? 'white' : '#e6e1e1', borderRadius: 10, padding: 15,
+       marginBottom: 5, borderColor: 'black', borderWidth: 1}} onPress={ReserveParking(item.id)}>
+        <Text style={{fontWeight: 'bold', color: 'black', alignSelf: 'flex-start'}}>
+          Parking {index + 1}
+          </Text>
+        <View style={{alignSelf: 'flex-end', alignItems: 'flex-end'}}>
+          <Text style={{color: 'black', alignSelf: 'flex-end', verticalAlign: 'bottom'}}>
+            {item.available ? 'Disponible' : 'Ocupado'}
+          </Text>
+        </View>
+      </View>
+    ));
+  }
+
   return (
     <View style={{alignItems: 'center', backgroundColor: 'white', flex: 1}}>
       <Text style={{fontSize: 16, fontWeight: 'bold', marginTop: 50, color:'black'}}>
