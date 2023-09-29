94c2665726e59d8ecbeff16cd949ee2cbf79c92f
Muirghen <52637101+Muirghen@users.noreply.github.com>
2023-09-28 22:21:31 -0400

    Applied email keyboard to email input

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -79,6 +79,7 @@ export default function App({navigation}) {
         style={styles.reducedMarginBtn}
         label='Email'
         mode='outlined'
+        keyboardType='email-address'
         onChangeText={(email) => setEmail(email)}
         />
       <TextInput
