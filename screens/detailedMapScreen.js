1a198287c43c59b4a4c255bc0032ed02dfa93d03
Muirghen <vasquezmariano01@gmail.com>
2023-10-05 17:45:15 -0400

    Parkings are now taken from db

--- screens/detailedMapScreen.js
+++ screens/detailedMapScreen.js
@@ -1,40 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
 import { Divider, List, Text } from 'react-native-paper';
+import axios from 'axios';
+import {IP_URL} from "@env";
 
-const parkings = [
-  {id : 1, available: true},
-  {id : 2, available: false},
-  {id : 3, available: true},
-  {id : 4, available: true},
-  {id : 5, available: false},
-  {id : 6, available: false},
-];
+export default function DetailedMapScreen({route, navigation}) {
+  const lotID = route.params.lotId;
+  const [parkingList, setParkingList] = useState([]);
+
+  useEffect(() => {
+    //console.log(lotID);
+    getAllParkings();
+  }, []);
+
+  const getAllParkings = () => {
+    const axiosUrl = `${IP_URL}parking/${lotID}`
+    axios.get(axiosUrl, { withCredentials: true })
+    .then(response => {
+        const newParkingList = [];
+        //console.log(response.data.body);
+        response.data.body.forEach(parking => {
+          newParkingList.push({value: parking._id, basePrice: parking.basePrice, name: parking.parking, parentLot: parking.parkingLot});
+        });
+        setParkingList(newParkingList);
+        //console.log(parkingList);
+    }
+    )
+    .catch(error => {
+      console.error(error);
+    })
+  }
 
 
-
-export default function DetailedMapScreen({navigation}) {
-  
   const ReserveParking = (id) => {
     navigation.navigate("Reservation", {parkingId: id});
   }
   
   const RenderParking = () => {
-    parkingSorted = parkings.sort((a, b) => b.available - a.available);
-    return parkingSorted.map((item, index) => (
-      <View style={{width: '90%', minHeight: 150,alignItems: 'center',
-       backgroundColor: item.available ? 'white' : '#e6e1e1', borderRadius: 10, padding: 15,
-       marginBottom: 5, borderColor: 'black', borderWidth: 1}} onPress={ReserveParking(item.id)}>
-        <Text style={{fontWeight: 'bold', color: 'black', alignSelf: 'flex-start'}}>
-          Parking {index + 1}
+    if (parkingList.length == 0) return (
+      <View>
+        <Text style={{fontSize: 16, fontWeight: 'bold', marginTop: 50, color:'gray'}}>
+          No Parkings available on this Lot
+        </Text>
+      </View>);
+    else {
+      parkingSorted = parkingList.sort((a, b) => b.available - a.available);
+      return parkingSorted.map((item, index) => (
+        <View style={{width: '90%', minHeight: 150,alignItems: 'center',
+        backgroundColor: item.available ? 'white' : '#e6e1e1', borderRadius: 10, padding: 15,
+        marginBottom: 5, borderColor: 'black', borderWidth: 1}} onPress={() => ReserveParking(item.id)}>
+          <Text style={{fontWeight: 'bold', color: 'black', alignSelf: 'flex-start'}}>
+            {item.name}
           </Text>
-        <View style={{alignSelf: 'flex-end', alignItems: 'flex-end'}}>
-          <Text style={{color: 'black', alignSelf: 'flex-end', verticalAlign: 'bottom'}}>
-            {item.available ? 'Disponible' : 'Ocupado'}
-          </Text>
+          <View style={{alignSelf: 'flex-end', alignItems: 'flex-end'}}>
+            <Text style={{color: 'black', alignSelf: 'flex-end', verticalAlign: 'bottom'}}>
+              {item.available ? 'Disponible' : 'Ocupado'}
+            </Text>
+          </View>
         </View>
-      </View>
-    ));
+      ));
+    }
   }
 
   return (
