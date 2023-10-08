ae421f0f5c924467d5c3a2730593553750212cf5
Muirghen <52637101+Muirghen@users.noreply.github.com>
2023-10-08 17:50:40 -0400

    Start of reservation system

--- screens/detailedMapScreen.js
+++ screens/detailedMapScreen.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
-import { Divider, List, Text } from 'react-native-paper';
+import { View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { Divider, List, Text, Modal, Portal, PaperProvider } from 'react-native-paper';
 import axios from 'axios';
 import {IP_URL} from "@env";
+import { TimePickerModal} from 'react-native-paper-dates';
 
 export default function DetailedMapScreen({route, navigation}) {
   const lotID = route.params.lotId;
   const [parkingList, setParkingList] = useState([]);
+  const [visible, setVisible] = React.useState(false);
+  const [vis_dateIn, setVis_dateIn] = React.useState(false);
+  const [vis_dateOut, setVis_dateOut] = React.useState(false);
+
+  const toggleModal = () => {
+    setVisible(!visible);
+  }
 
   useEffect(() => {
     //console.log(lotID);
@@ -46,30 +54,33 @@ export default function DetailedMapScreen({route, navigation}) {
     else {
       parkingSorted = parkingList.sort((a, b) => b.available - a.available);
       return parkingSorted.map((item, index) => (
-        <View style={{width: '90%', minHeight: 150,alignItems: 'center',
-        backgroundColor: item.available ? 'white' : '#e6e1e1', borderRadius: 10, padding: 15,
-        marginBottom: 5, borderColor: 'black', borderWidth: 1}} onPress={() => ReserveParking(item.id)}>
-          <Text style={{fontWeight: 'bold', color: 'black', alignSelf: 'flex-start'}}>
+        <TouchableOpacity style={{width: '90%', minHeight: 150,alignItems: 'center',
+        backgroundColor: 'white', borderRadius: 10, padding: 15, justifyContent: 'center',
+        marginBottom: 5, borderColor: 'black', borderWidth: 1}} onPress={() => toggleModal()}>
+          <Text style={{fontWeight: 'bold', color: 'black', alignSelf: 'center', fontSize: 50}}>
             {item.name}
           </Text>
-          <View style={{alignSelf: 'flex-end', alignItems: 'flex-end'}}>
-            <Text style={{color: 'black', alignSelf: 'flex-end', verticalAlign: 'bottom'}}>
-              {item.available ? 'Disponible' : 'Ocupado'}
-            </Text>
-          </View>
-        </View>
+        </TouchableOpacity>
       ));
     }
   }
 
   return (
-    <View style={{alignItems: 'center', backgroundColor: 'white', flex: 1}}>
+    <PaperProvider>
+      <View style={{alignItems: 'center', backgroundColor: 'white', flex: 1}}>
       <Text style={{fontSize: 16, fontWeight: 'bold', marginTop: 50, color:'black'}}>
         INTEC - Parqueo de los profesores
       </Text>
       <ScrollView contentContainerStyle={{alignItems: 'center'}} style={{width: '100%', height: '70%'}}>
           {RenderParking()}
-        </ScrollView>
+      </ScrollView>
+      <Portal>
+        <Modal visible={visible} onDismiss={() => {toggleModal()}} contentContainerStyle={{backgroundColor: 'white', padding: 20, alignItems: 'center'}}>
+          <Text onPress={showIn}>{/*dateIn.toLocaleDateString()*/}{dateIn}</Text>
+          <Text onPress={showOut}>{/*dateOut.toLocaleDateString()*/}{dateOut}</Text>
+        </Modal>
+      </Portal>
     </View>
+    </PaperProvider>
   );
 };
